57386df24ae450b53796990fb14c0f3dd26d355b
Mohamed Belal <111300150+Mohamedbelal17@users.noreply.github.com>
2026-05-13 13:03:29 +0300

    db.js change
    Removed commented-out connect and query function.

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,20 +4,6 @@ const config = require('./dbconfig');
 
 let pool = null;
 
-// async function connectandquery(){
-//     try {
-//         let pool = await sql.connect(config);
-//         let result = await pool.request().query('select * from students');
-//         console.log(result);
-//     } catch(error){
-//         console.log('error is :', error);
-//     }finally {
-//         await sql.close();
-//     }
-// }
-
-// connectandquery();
-
 async function getPool() {
     if (!pool) {
         pool = await sql.connect(config);
